47a2420d820e5c1897bde3773043a12527ce41df
Maikx <mikeybee2000@gmail.com>
2020-11-29 19:45:44 +0100

    Added player and view from player

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController cC;
+
+    public float speed;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+        Gravity();
+        Jump();
+    }
+
+    public void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        cC.Move(move * speed * Time.deltaTime);
+    }
+
+    public void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance
+            , groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        cC.Move(velocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
